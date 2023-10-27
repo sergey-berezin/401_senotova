16ce4d335c2cca38fc0ff61600c707e8a9396c16
YuliyaSenotova <yulsenva@gmail.com>
2023-10-27 23:54:15 +0300

    Добавлен Task2. В нем не работает прокрутка ListView и в выводе значение степени уверенности константное, потому что оно считалось в функции nuget package =, и в первой версии не сохранялось, я изменила nuget package, но программа почему не видит измененный nuget package.

---
+++ Task2A/Task2A/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Windows.Markup;
+using System.Windows.Forms;
+using ViewModel;
+using static System.Net.WebRequestMethods;
+using SixLabors.ImageSharp.PixelFormats;
+using AsyncCommand;
+using System.Reflection.Emit;
+using SixLabors.ImageSharp;
+using System.Reflection.Metadata;
+
+namespace Task2A
+{
+    public class CustomCommands
+    {
+        public static RoutedCommand ChooseFromFileCommand =
+                new RoutedCommand("Get directory", typeof(CustomCommands));
+        public static RoutedCommand CalculateCommand =
+                new RoutedCommand("Images from directory", typeof(CustomCommands));
+        /*<CommandBinding Command="{x:Static local:CustomCommands.ChooseFromFileCommand}"
+                CanExecute = "CorrectDirectoryName" Executed="FromDirectoryClick"/>
+        <CommandBinding Command="{x:Static local:CustomCommands.CalculateCommand}"
+                CanExecute = "CorrectImagesDirectory" Executed="FromDirectoryExecute"/>*/
+    }
+    public partial class MainWindow : Window
+    {
+        DataModel viewData = new DataModel();
+        //private ICommand startCommand;
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*startCommand = new AsyncRelayCommand(async _ =>
+            {
+                System.Windows.Forms.MessageBox.Show("in command");
+                viewData.CreateList();
+
+            });*/
+            this.DataContext = viewData;
+            //function_list.ItemsSource = Enum.GetValues(typeof(ClassLibrary1.FRawEnum));
+        }
+        //public ICommand StartCommand => startCommand;
+        private void CorrectDirectoryName(object sender, CanExecuteRoutedEventArgs e)
+        {
+            try
+            {
+                e.CanExecute = true;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                e.CanExecute = false;
+            }
+        }
+
+        private void FromDirectoryClick(object sender, ExecutedRoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            DialogResult result = folderBrowser.ShowDialog();
+
+            string[]? files = null;
+            if (!string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+            {
+                files = Directory.GetFiles(folderBrowser.SelectedPath);
+                viewData.LoadFiles(files);
+                DataContext = null;
+                DataContext = viewData;
+            }
+        }
+        private void CorrectImagesDirectory(object sender, CanExecuteRoutedEventArgs e)
+        {
+            try
+            {
+                var files = viewData.filenames;
+                bool can_execute = true;
+                if (files.Count > 0)
+                {
+                    foreach (var file in files)
+                    {
+                        can_execute = file.EndsWith(".jpg") ? can_execute : false;
+                    }
+                }
+                else
+                {
+                    can_execute = false;
+                }
+                e.CanExecute = can_execute;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+                e.CanExecute = false;
+            }
+        }
+
+        private bool isExecuting;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        //private ICommand startCommand;
+        private async void FromDirectoryExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            viewData.LoadImages();
+            //viewData.ImagesToList();
+
+            //System.Windows.Forms.MessageBox.Show("in executing");
+            //if (!isExecuting)
+            //{
+            //    isExecuting = true;
+            //await Task.Delay(1000);
+            await viewData.CreateList();/*.ContinueWith(_ =>
+                {
+                    isExecuting = false;
+                    CommandManager.InvalidateRequerySuggested();
+                }, scheduler: TaskScheduler.FromCurrentSynchronizationContext());
+            //}*/
+            DataContext = null;
+            DataContext = viewData;
+            //viewData.ImagesToList();
+            //await viewData.ProccessImages();
+            /*startCommand = new AsyncRelayCommand(async _ =>
+            {
+                await viewData.ProcessImages();
+            });
+            public ICommand StartCommand => startCommand;
+            //await StartCommand;
+            DataContext = null;
+            DataContext = viewData;
+            */
+        }
+        private void usersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var item = sender as ListData;// System.Windows.Forms.ListViewItem;
+            if (ListImages.SelectedItem is ListData)// && item. IsSelected())
+            {
+                viewData.ChosenImage = ((ListData)ListImages.SelectedItem).image.Clone();
+                //System.Windows.Forms.MessageBox.Show("changed!");
+            }
+            DataContext = null;
+            DataContext = viewData;
+        }
+    }
+}
---
+++ Task2A/ViewModel/DataModel.cs
@@ -0,0 +1,107 @@
+﻿using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using System.Collections.Generic;
+using SixLabors.Fonts;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.CompilerServices;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using ONNX_detect;
+namespace ViewModel
+{
+    public class ListData
+    { 
+        public string ImagePath { get; set; }
+        public Image<Rgb24> image { get; set; }
+        public string Label { get; set; }
+        public float conf { get; set; }
+        public ListData(string path, Image<Rgb24> image_init, string label, float confidence)
+        {
+            this.ImagePath = (string)path.Clone();
+            this.conf = confidence;
+            this.image = image_init.Clone();
+            this.Label = (string)label.Clone();
+        }
+    }
+    public class DataModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] String propertyName = "") =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public List<Image<Rgb24>> loaded_images { get; set; }
+        public string[]? files { get; set; }
+        public List<string> filenames { get; set; }// = new List<string>(arr);
+        //string[]? filenames { get; set; }
+        public List<string> FileNames => filenames;
+        public List<ListData> items { get; set; }
+        public Image<Rgb24>? ChosenImage;
+
+        public DataModel()//List<Image<Rgb24>> images, List<string> filenames, )
+        {
+            files = null;
+            filenames = new List<string>();
+            loaded_images = new List<Image<Rgb24>>();
+            items = new List<ListData>();
+            ChosenImage = null;
+        }
+        public void LoadFiles(string[] files)
+        {
+            if (files != null)
+            {
+                this.files = (string[])files.Clone();
+                filenames = new List<string>(files);
+            }
+        }
+        public void LoadImages()
+        {
+            if(filenames.Count > 0)
+            {
+                //filenames = (string[])files.Clone();
+                for (int i = 0; i < filenames.Count(); i++)
+                {
+                    try
+                    {
+                        var image = SixLabors.ImageSharp.Image.Load<Rgb24>(filenames[i]);
+                        loaded_images.Add(image);
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                }
+            }
+        }
+        public void ImagesToList()
+        {
+            for(int i = 0; i < loaded_images.Count(); ++i)
+            {
+                items.Add(new ListData(filenames[i], loaded_images[i], "label", (float)0.1));
+            }
+        }
+        public async Task CreateList()
+        {
+            (List<Image<Rgb24>>, List<DataItem>) results;
+
+            //results = await ClassDetection.ObjectDetection(loaded_images, files);
+            /*for(int i = 0; i < results.Item1.Count; ++i)
+            {
+                double x = (double)results.Item2[i].x;
+                var y = results.Item2[i].y;
+                var w = results.Item2[i].w;
+                var h = results.Item2[i].h;
+                var name = results.Item2[i].filename;
+                var label = results.Item2[i].label;
+                //var rec = new Rectangle((int)x, (int)y, (int)w, (int)h);
+                var segment = results.Item1[i].Clone(j => j.Crop(rec));
+                items.Add(new ListData(name, loaded_images[i], label, (float)1.0));
+            }*/
+            items.Add(new ListData(filenames[0], loaded_images[0], "label", (float)0.1));
+            items.Add(new ListData(filenames[1], loaded_images[1], "label", (float)0.1));
+        }
+    }
+}
